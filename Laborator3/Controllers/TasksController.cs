eb9034e752ef9e1be9ae9c0d49962aca76f41dba
agtourcj3 <lucian.maniu@gmail.com>
2019-05-18 14:25:41 +0300

    Added comments and swagger documentation

--- Laborator3/Controllers/TasksController.cs
+++ Laborator3/Controllers/TasksController.cs
@@ -20,6 +20,13 @@ namespace Laborator3.Controllers
         }
 
         // GET: api/Tasks
+        /// <summary>
+        /// Gets all the tasks in the database.
+        /// </summary>
+        
+        /// <param name="from">Optional, filter by minimum deadline.</param>
+        /// <param name="to">Optional, filter by maximum deadline.</param>
+        /// <returns>A list of Task objects.</returns>
         [HttpGet]
         public IEnumerable<Models.Task> Get([FromQuery]DateTime? from, [FromQuery]DateTime? to)
         {
@@ -38,6 +45,12 @@ namespace Laborator3.Controllers
         }
 
         // GET: api/Tasks/5
+        /// <summary>
+        /// Get a task by a given ID.
+        /// </summary>
+        /// 
+        /// <param name="id">Task ID</param>
+        /// <returns>A task with a given ID</returns>
         [HttpGet("{id}", Name = "Get")]
         public IActionResult Get(int id)
         {
@@ -51,7 +64,40 @@ namespace Laborator3.Controllers
         }
 
         // POST: api/Tasks
+        /// <summary>
+        /// Add a task
+        /// </summary>
+        /// /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /tasks
+        ///    {
+        ///         "title": "Expedia Conference",
+        ///         "description": "Attending Expedia Conference at Hilton",
+        ///         "dateAdded": "2019-05-17T10:08:57.3616694",
+        ///         "deadline": "2019-06-01T12:00:00",
+        ///         "taskImportance": 2,
+        ///         "taskState": 0,
+        ///         "dateClosed": null,
+        ///         "comments": [
+        ///	            {
+        ///		            "text": "Write down some important questions!",
+        ///		            "important": true
+        ///             },
+        ///	            {
+        ///		            "text": "Get all the relevant info for the market!",
+        ///		            "important": false
+        ///	            }
+        ///	                    ]
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="task">The task to add.</param>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>
         [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
         public void Post([FromBody] Models.Task task)
         {
             task.DateClosed = null;
@@ -61,6 +107,12 @@ namespace Laborator3.Controllers
         }
 
         // PUT: api/Tasks/5
+        /// <summary>
+        /// Update a task with the given ID, or create a new task if the ID does not exist.
+        /// </summary>
+        /// <param name="id">task ID</param>
+        /// <param name="task">The object Task</param>
+        /// <returns>The updated task/new created task.</returns>
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] Models.Task task)
         {
@@ -85,6 +137,11 @@ namespace Laborator3.Controllers
         }
 
         // DELETE: api/ApiWithActions/5
+        /// <summary>
+        /// Delet a task by a given ID
+        /// </summary>
+        /// <param name="id">ID of the task to be deleted.</param>
+        /// <returns>The deleted task object.</returns>
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
--- Laborator3/Startup.cs
+++ Laborator3/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,11 +13,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Laborator3
 {
     public class Startup
     {
+        //https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-2.2&tabs=visual-studio
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,7 +32,36 @@ namespace Laborator3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContext<Models.TasksDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<Models
+                .TasksDbContext>(options => options
+                .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "version 1.1",
+                    Title = "Tasks Management Web API",
+                    Description = "Swagger for Web API - Tasks",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Lucian Maniu",
+                        Email = string.Empty,
+                        Url = ""
+                    },
+                    License = new License
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +78,16 @@ namespace Laborator3
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
